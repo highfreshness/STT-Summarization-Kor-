e1db1d80c59f34b733034264bf8211398305e3a9
highfreshness <meteo0718@gmail.com>
2022-09-29 16:09:35 +0900

    rollback

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import os
+import time
 import streamlit as st
 from stt import stt_wav
 from audiorecorder import audiorecorder
 from summarization import summarization_text
-from pydub import AudioSegment
-
-
 
 st.markdown("# **🎤 STT(Speech To Text) & ⭐Summarization**")
 
@@ -13,20 +11,16 @@ option = st.selectbox("입력 방식을 선택해주세요", ("마이크", ".wav
 
 if option == "마이크":
     audio = audiorecorder("녹음 시작", "녹음 중...")
-
     if len(audio) > 0:
     # To play audio in frontend:
         st.audio(audio)
         # To save audio to a file:
         wav_file = open("audio.mp3", "wb")
         wav_file.write(audio.tobytes())
-        ###
-        AudioSegment.from_mp3("./audio.mp3").export("./src.wav", format="wav")
-        ###
-        # commandwav = "ffmpeg -y -i audio.mp3 audio.wav"
-        # os.system(commandwav)
+        commandwav = "ffmpeg -y -i audio.mp3 src.wav"
+        os.system(commandwav)
         try:
-            preprocessing = stt_wav("src.wav")
+            preprocessing = stt_wav("./src.wav")
             st.markdown("### 🔑 대화 내용 요약")
             with st.spinner('텍스트를 요약하는 중 입니다.'):
                 st.code(summarization_text(preprocessing), language='python')
@@ -46,6 +40,6 @@ elif option == ".wav 파일 업로드":
 # 사용했던 음성 파일 삭제
 try:
     os.remove("audio.mp3")
-    os.remove("audio.wav")
+    os.remove("src.wav")
 except FileNotFoundError:
     pass
