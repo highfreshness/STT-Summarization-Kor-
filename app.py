10a220e8faad54aaaa6ad398763b07e63e599d64
highfreshness <meteo0718@gmail.com>
2022-09-29 12:00:41 +0900

    기존 잔여 음성 파일 삭제 및 예외처리

--- app.py
+++ app.py
@@ -5,22 +5,28 @@ from summarization import summarization_text
 from stt import stt_wav
 
 
-st.markdown("## **🎤 STT(Speech To Text) & ⭐Summarization**")
+st.markdown("# **🎤 STT(Speech To Text) & ⭐Summarization**")
 
 option = st.selectbox("입력 방식을 선택해주세요", ("마이크", ".wav 파일 업로드"))
 
 if option == "마이크":
     audio = audiorecorder("녹음 시작", "녹음 중...")
+
     if len(audio) > 0:
-        # To play audio in frontend:
+    # To play audio in frontend:
         st.audio(audio)
         # To save audio to a file:
         wav_file = open("audio.mp3", "wb")
         wav_file.write(audio.tobytes())
         commandwav = "ffmpeg -y -i audio.mp3 audio.wav"
         os.system(commandwav)
-        preprocessing = stt_wav("audio.wav")
-        st.code(summarization_text(preprocessing), language='python')
+        try:
+            preprocessing = stt_wav("audio.wav")
+            st.markdown("### 🔑 대화 내용 요약")
+            with st.spinner('텍스트를 요약하는 중 입니다.'):
+                st.code(summarization_text(preprocessing), language='python')
+        except FileNotFoundError:
+            st.error("음성이 입력되지 않았습니다. 다시 입력해주세요", icon="🚨")
 
 elif option == ".wav 파일 업로드":
     try:
@@ -31,7 +37,10 @@ elif option == ".wav 파일 업로드":
             st.code(summarization_text(preprocessing), language='python')
     except AssertionError:
         pass
-# text = """
-# 올 여름 혼자어때 둘이어때 셋이어때 올여름 여행어때 바다어때 여기어때
-# 여행할 때 여기어때 여행어때 여기어때
-# """
+
+# 사용했던 음성 파일 삭제
+try:
+    os.remove("audio.mp3")
+    os.remove("audio.wav")
+except FileNotFoundError:
+    pass
